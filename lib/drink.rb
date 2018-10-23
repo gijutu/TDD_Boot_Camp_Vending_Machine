43551f72ff1362398dd24ebc010d009f591f152d
Masahiro Nishimi <nishimi@sonicgarden.jp>
2018-10-23 15:58:07 +0900

    2018/10/23レビュー

--- lib/drink.rb
+++ lib/drink.rb
@@ -10,4 +10,20 @@ class Drink
     { name: @name,
     price: @price }
   end
+
+  def to_s
+    "#{@name}#{@price}円"
+  end
+
+  def self.coke
+    @coke ||= Drink.new("コーラ", 120)
+  end
+
+  def self.red_bull
+    @red_bull ||= Drink.new("レッドブル",200)
+  end
+
+  def self.water
+    @water ||= Drink.new("水", 100)
+  end
 end
